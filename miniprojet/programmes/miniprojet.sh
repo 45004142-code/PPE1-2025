7a008fb49ef84c50ea3daeba6925ac1be985a9e1
이예지 <iyeji@MacBookAir.lan>
2025-11-10 21:00:55 +0100

    miniprojet-2

--- miniprojet/programmes/miniprojet.sh
+++ miniprojet/programmes/miniprojet.sh
@@ -1,25 +1,35 @@
-if [ $# -ne 1 ];
-then
-	echo "ce programme demande un argument"
-	exit 1
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+    echo "ce programme demande un argument"
+    exit 1
 fi
 
 FICHIER_URLS=$1
 N=0
 
-while read -r line;
-do
-	N=$(expr $N + 1)
+mkdir -p tableaux
 
-	# HTTP
-	CODE=$(curl -s -o/dev/null -w "%{http_code}" "$line")
+# HTML 테이블 헤더
+echo "<table border=\"1\">" > tableaux/tableau-fr.html
+echo "<tr><th>N°</th><th>URL</th><th>HTTP Code</th><th>Encoding</th><th>Word Count</th></tr>" >> tableaux/tableau-fr.html
 
-	# Encoding
-	ENCODING=$(curl -i -s -L "$line" | grep -i "charset")
-	
-	# Nombre de mots
-	WORDS=$(curl -s -L "$line" | wc -w)
-	
-	echo -e "${N}\t${line}\t${CODE}\t${ENCODING}\t${WORDS}" >> tableaux/tableau-fr.tsv
+while read -r line; do
+    N=$((N+1))
+
+    # HTTP code
+    CODE=$(curl -s -o /dev/null -w "%{http_code}" "$line")
+
+    # Encoding
+    ENCODING=$(curl -s -I "$line" | grep -i "Content-Type" | awk -F'charset=' '{print $2}' | tr -d '\r')
+    [ -z "$ENCODING" ] && ENCODING="NA"
+
+    # Nombre de mots (HTML 태그 제거)
+    WORDS=$(curl -s -L "$line" | sed 's/<[^>]*>//g' | wc -w)
+
+    # HTML 테이블 row 추가
+    echo "<tr><td>${N}</td><td>${line}</td><td>${CODE}</td><td>${ENCODING}</td><td>${WORDS}</td></tr>" >> tableaux/tableau-fr.html
 
 done < "$FICHIER_URLS"
+
+echo "</table>" >> tableaux/tableau-fr.html
